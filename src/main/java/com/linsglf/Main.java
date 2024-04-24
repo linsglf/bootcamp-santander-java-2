96d77e296eac0f4db3dbd9ef208aa37d87856682
Gabriel Lins <101433324+linsglf@users.noreply.github.com>
2024-04-24 01:48:14 -0300

    Pronto

--- src/main/java/com/linsglf/Main.java
+++ src/main/java/com/linsglf/Main.java
@@ -1,17 +1,50 @@
 package com.linsglf;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class ParametrosInvalidosException extends Exception{
+
+    public ParametrosInvalidosException (String menssageErr){
+        super(menssageErr);
+    }
+
+} 
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in).useLocale(Locale.US);
+        
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException e) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+           System.out.println(e.getMessage());
+		}
+		
+	}
+
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contage
+
+        if(parametroUm < parametroDois){
+            for (int index = 1; index <= contagem; index++) {
+                System.out.println("Imprimindo o número " + index);
+    
+            }
+        }else{
+            String error = "O segundo parâmetro deve ser maior que o primeiro!";
+            throw new ParametrosInvalidosException(error);
         }
     }
+
 }
